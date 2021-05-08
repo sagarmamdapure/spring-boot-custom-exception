4ee8461c2b525ca1ae3f92c48ecd279c58573e6c
Sagar Mamdapure <sagarmamdapure@gmail.com>
2021-05-08 14:12:44 +0530

    added ApiException class

--- src/main/java/com/example/customexception/exceptions/ApiException.java
+++ src/main/java/com/example/customexception/exceptions/ApiException.java
@@ -1,2 +1,15 @@
-package com.example.customexception.exceptions;public class ApiException {
+package com.example.customexception.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.time.ZonedDateTime;
+
+@AllArgsConstructor
+@Getter
+public class ApiException {
+  private final String message;
+  private final HttpStatus httpStatus;
+  private final ZonedDateTime timestamp;
 }

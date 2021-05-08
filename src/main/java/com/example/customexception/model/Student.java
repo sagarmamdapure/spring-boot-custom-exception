90df9a32b92f65fe8f59e99a10f79f57d116c650
Sagar Mamdapure <sagarmamdapure@gmail.com>
2021-05-08 13:44:22 +0530

    added model class to hold student related data

--- src/main/java/com/example/customexception/model/Student.java
+++ src/main/java/com/example/customexception/model/Student.java
@@ -1,2 +1,23 @@
-package com.example.customexception.model;public class Student {
+package com.example.customexception.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    @Id
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+
 }

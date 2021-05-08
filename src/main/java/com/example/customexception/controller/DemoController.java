7f4356ad8af49e1475a83254429c94e4b4748688
Sagar Mamdapure <sagarmamdapure@gmail.com>
2021-05-08 13:43:51 +0530

    added rest controller

--- src/main/java/com/example/customexception/controller/DemoController.java
+++ src/main/java/com/example/customexception/controller/DemoController.java
@@ -1,2 +1,18 @@
-package com.example.customexception.controller;public class DemoController {
+package com.example.customexception.controller;
+
+import com.example.customexception.model.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("student")
+public class DemoController {
+
+    @GetMapping
+    public List<Student> getStudent() {
+        throw new IllegalStateException("Oops exception occurred");
+    }
 }

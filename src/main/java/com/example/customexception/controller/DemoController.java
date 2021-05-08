93f749d0682e671181f9012a7d50c7b0cfb1878e
Sagar Mamdapure <sagarmamdapure@gmail.com>
2021-05-08 14:09:17 +0530

    implemented custom exception

--- src/main/java/com/example/customexception/controller/DemoController.java
+++ src/main/java/com/example/customexception/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.example.customexception.controller;
 
+import com.example.customexception.exceptions.ApiRequestException;
 import com.example.customexception.model.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +12,8 @@ import java.util.List;
 @RequestMapping("student")
 public class DemoController {
 
-    @GetMapping
-    public List<Student> getStudent() {
-        throw new IllegalStateException("Oops exception occurred");
-    }
+  @GetMapping
+  public List<Student> getStudent() {
+    throw new ApiRequestException("Oops exception occurred");
+  }
 }
--- src/main/java/com/example/customexception/model/Student.java
+++ src/main/java/com/example/customexception/model/Student.java
@@ -5,19 +5,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
-@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Student {
-    @Id
-    private Long id;
-    private String firstName;
-    private String lastName;
-
-
+  private Long id;
+  private String firstName;
+  private String lastName;
 }
